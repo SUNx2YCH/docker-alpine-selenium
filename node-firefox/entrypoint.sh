7f2bbb33b75687a40d645903e4f357dbf49d804e
Alexander Ivanovsky <sunx2ych@gmail.com>
2015-12-06 19:32:54 +0300

    Draft version of node-firefox

---
+++ node-firefox/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+trap teardown SIGINT SIGTERM
+
+teardown()
+{
+	kill -s SIGTERM $JAVA_PID
+	wait $JAVA_PID
+	exit 0
+}
+
+# http://blog.jeffterrace.com/2012/07/xvfb-memory-leak-workaround.html
+# https://gist.github.com/jterrace/2911875
+# https://github.com/LoicMahieu/alpine-wkhtmltopdf/blob/master/Dockerfile
+export DISPLAY=":0.0"
+Xvfb :0 -screen 0 1280x800x24 -ac +extension GLX +render -noreset > /dev/null 2>&1 &
+
+java -jar selenium-server-standalone-2.48.2.jar -role node \
+                                                -hub http://$HUB_HOST:$HUB_PORT/grid/register \
+                                                -browser browserName=firefox,version=38.3,maxInstances=1,platform=LINUX &
+JAVA_PID=$!
+wait $JAVA_PID
